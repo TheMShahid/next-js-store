fa01d4af8feb1dc0699b04edfafd37026daa06e3
Muhammad Shahid Khan <shahidlanger0@gmail.com>
2025-10-24 22:36:46 +0500

    created the theme component, dropdown, navbar and also added the shadcui library

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import { Button } from "@/components/ui/button";
 function HomePage() {
   return (
     <div>
-      <h1 className="text-3xl">Home page</h1>
+      <h1 className="text-3xl text-muted-foreground">Home page</h1>
       <Button variant="outline" size="lg" className="capitalize mt-8">
         click me
       </Button>
