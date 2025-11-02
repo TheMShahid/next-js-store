040973d9f5fd7bc49d34f79c4702deb20030debc
Muhammad Shahid Khan <shahidlanger0@gmail.com>
2025-11-02 19:58:29 +0500

    updated and fixed the product details or single product page

--- app/products/[id]/page.tsx
+++ app/products/[id]/page.tsx
@@ -6,8 +6,15 @@ import { fetchSingleProduct } from "@/utils/actions";
 import { formatCurrency } from "@/utils/format";
 import Image from "next/image";
 
-async function SingleProductPage({ params }: { params: { id: string } }) {
-  const product = await fetchSingleProduct(params.id);
+async function SingleProductPage({
+  params,
+}: {
+  params: Promise<{ id: string }>;
+}) {
+  console.log("check_params", params);
+  const { id } = await params;
+
+  const product = await fetchSingleProduct(id);
   const { name, image, price, company, description } = product;
   const dollarsAmount = formatCurrency(price);
 
@@ -30,15 +37,15 @@ async function SingleProductPage({ params }: { params: { id: string } }) {
         <div>
           <div className="flex gap-x-8 items-center">
             <h1 className=" capitalize text-3xl font-bold">{name}</h1>
-            <FavoriteToggleButton productId={params.id} />
+            <FavoriteToggleButton productId={id} />
           </div>
-          <ProductRating productId={params.id} />
+          <ProductRating productId={id} />
           <h4 className="text-xl mt-2">{company}</h4>
           <p className="mt-3 text-md bg-muted inline-block p-2 rounded">
             {dollarsAmount}
           </p>
           <p className="mt-6 leading-8 text-muted-foreground">{description}</p>
-          <AddToCart productId={params.id} />
+          <AddToCart productId={id} />
         </div>
       </div>
     </section>
